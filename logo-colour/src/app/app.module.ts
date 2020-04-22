a4e7448952b9f856afb52e4218f03849ac18a845
beckham <beckham.lam@mail.mcgill.ca>
2020-04-21 20:04:05 -0400

    feat(lazy loaded images)

--- logo-colour/src/app/app.module.ts
+++ logo-colour/src/app/app.module.ts
@@ -1,51 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IconCardComponent } from './dashboard/icon-card/icon-card.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatRadioModule } from '@angular/material/radio';
-import { LogoFilterPipe } from './pipes/logo-search.pipe';
-import { ColorPickerModule } from 'ngx-color-picker';
-import { PopupDirective } from './directives/popup-directive';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { IconCardComponent } from "./dashboard/icon-card/icon-card.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClipboardModule } from "@angular/cdk/clipboard";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatRadioModule } from "@angular/material/radio";
+import { LogoFilterPipe } from "./pipes/logo-search.pipe";
+import { ColorPickerModule } from "ngx-color-picker";
+import { PopupDirective } from "./directives/popup-directive";
+import { LazyLoadImageModule } from "ng-lazyload-image";
 @NgModule({
-   declarations: [
-      AppComponent,
-      DashboardComponent,
-      IconCardComponent,
-      LogoFilterPipe,
-      PopupDirective
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      FlexLayoutModule,
-      BrowserAnimationsModule,
-      ClipboardModule,
-      MatSnackBarModule,
-      MatFormFieldModule,
-      MatSelectModule,
-      MatInputModule,
-      ReactiveFormsModule,
-      FormsModule,
-      MatToolbarModule,
-      MatRadioModule,
-      ColorPickerModule
-   ],
-   exports: [IconCardComponent, PopupDirective],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    IconCardComponent,
+    LogoFilterPipe,
+    PopupDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    ClipboardModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatRadioModule,
+    ColorPickerModule,
+    LazyLoadImageModule
+  ],
+  exports: [IconCardComponent, PopupDirective],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- logo-colour/src/app/dashboard/icon-card/icon-card.component.ts
+++ logo-colour/src/app/dashboard/icon-card/icon-card.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { LogoModel } from '../../models/logo_model';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit, Input } from "@angular/core";
+import { LogoModel } from "../../models/logo_model";
+import { MatSnackBar } from "@angular/material/snack-bar";
 @Component({
-  selector: 'app-icon-card',
-  templateUrl: './icon-card.component.html',
-  styleUrls: ['./icon-card.component.scss']
+  selector: "app-icon-card",
+  templateUrl: "./icon-card.component.html",
+  styleUrls: ["./icon-card.component.scss"]
 })
 export class IconCardComponent implements OnInit {
-
   @Input()
   app: LogoModel;
-  
-  constructor(private snackBar: MatSnackBar) { }
+
+  image: string;
+
+  constructor(private snackBar: MatSnackBar) {}
 
   ngOnInit() {
+    this.image = "../../../assets/images/" + this.app.img;
   }
 
   onClickAddToClipboard(clickElement: string) {
-    this.snackBar.open('Copied ' + clickElement + ' to the clipboard!', 'Close', {
-      duration: 2000
-    });
+    this.snackBar.open(
+      "Copied " + clickElement + " to the clipboard!",
+      "Close",
+      {
+        duration: 2000
+      }
+    );
   }
-
 }
